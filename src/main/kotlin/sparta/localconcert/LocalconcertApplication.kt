bfdaa7033af1d9396ad51e3ac6ec13d651b7b1ab
재영이 <jeayeong70@gmail.com>
2024-02-14 20:13:36 +0900

    초기ã파일

---
+++ src/main/kotlin/sparta/localconcert/LocalconcertApplication.kt
@@ -0,0 +1,11 @@
+package sparta.localconcert
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LocalconcertApplication
+
+fun main(args: Array<String>) {
+	runApplication<LocalconcertApplication>(*args)
+}
---
+++ src/test/kotlin/sparta/localconcert/LocalconcertApplicationTests.kt
@@ -0,0 +1,13 @@
+package sparta.localconcert
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LocalconcertApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
